958e933665d0073314901c345239ac66780b8869
Agustina Fraccaro <afraccaro@fi.uba.ar>
2021-10-20 16:27:37 -0300

    cambios al main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from grafo import Grafo
-from Dijkstra importa dijkstra 
-from BellmanFord import bellman_ford
+from johnson import johnson
 
 mapa_depositos = Grafo(True)
 with open("depositos.txt", "r") as archivo:
@@ -10,8 +9,8 @@ with open("depositos.txt", "r") as archivo:
 		mapa_depositos.agregar_vertice(info[1])
 		mapa_depositos.agregar_arista(info[0],info[1],int(info[2]))
 
-mapa_depositos.agregar_vertice("inicio")
-for nodo in mapa_depositos:
-	mapa_depositos.agregar_arista("inicio", nodo, 0)
-
-bellman_ford(mapa_depositos, "inicio")
+distancias_minimas = johnson(mapa_depositos)
+sumas = {}
+for i in distancias_minimas:
+	for j in distancias_minimas[i]:
+		sumas[i] += distancias_minimas[i][j] 
