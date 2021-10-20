09c65840c8839e153ea0911a19a2a1899635e5d7
Agustina Fraccaro <afraccaro@fi.uba.ar>
2021-10-20 17:08:40 -0300

    agrego lo de encontrar la ciudad donde va el depo

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from grafo import Grafo
 from johnson import johnson
 
-mapa_depositos = Grafo(True)
+mapa_ciudades = Grafo(True)
 with open("depositos.txt", "r") as archivo:
 	for linea in archivo:
 		info = linea.replace("\r\n","").split(',')
-		mapa_depositos.agregar_vertice(info[0])
-		mapa_depositos.agregar_vertice(info[1])
-		mapa_depositos.agregar_arista(info[0],info[1],int(info[2]))
+		mapa_ciudades.agregar_vertice(info[0])
+		mapa_ciudades.agregar_vertice(info[1])
+		mapa_ciudades.agregar_arista(info[0],info[1],int(info[2]))
 
-distancias_minimas = johnson(mapa_depositos)
+distancias_minimas = johnson(mapa_ciudades)
+
 sumas = {}
 for i in distancias_minimas:
+	sumas[i] = 0
 	for j in distancias_minimas[i]:
-		sumas[i] += distancias_minimas[i][j] 
+		sumas[i] = sumas[i] + distancias_minimas[i][j]
+
+
+ciudad_elegida = min(sumas, key=sumas.get)
+print(ciudad_elegida)
